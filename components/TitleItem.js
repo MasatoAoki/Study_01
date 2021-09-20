05cf53f12f280ff603e156f7433e08c42750b614
masato <aoki.masato.lmasato@gmail.com>
2021-09-20 22:50:27 +0900

    cg_202109202250

---
+++ components/TitleItem.js
@@ -0,0 +1,35 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, FlatList, SafeAreaView, Button, Alert } from 'react-native';
+
+
+const TitleItem = (props) => {
+    return (
+        <View style={styles.TitleContainer}>
+            <Text style={styles.Title}> {props.title}</Text>
+        </View>
+
+    )
+}
+
+
+const styles = StyleSheet.create({
+
+    TitleContainer: {
+        backgroundColor: "#fff",
+        width: "100%",
+        height: "10%",
+        alignItems: "center",
+        justifyContent: 'center',
+    },
+
+    Title: {
+        fontSize: 20,
+        fontWeight: "700"
+
+    },
+
+});
+
+
+export default TitleItem;
---
+++ components/TubItem.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, FlatList, SafeAreaView, Button, Alert } from 'react-native';
+
+
+const TubItem = () => {
+    return (
+
+        <View style={styles.tubContainer}></View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+
+    tubContainer: {
+        width: "100%",
+        height: "12%",
+        backgroundColor: "#fff",
+        borderWidth: 1,
+    },
+
+
+});
+
+
+export default TubItem;
